57eb411c97d45df43f9ab6e59c918ee6f2270790
Michael <m.molin.1987@gmail.com>
2020-02-28 16:59:30 +0100

    Menù a tendina completato con metodo(add/remove (class Active) e con metodo slideDown/up. Nel fine settimana creazione freccia animata

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    
+
+    //  ESERCIZIO CREATO CON AGGIUNTA CLASSE ACITVE (DISPLAY: NONE/BLOCK)
+    // $('.menu-item').click(function() {
+    //     if ($(this).children('.dropdown-menu').is(':visible')) {
+    //             $('.dropdown-menu').removeClass('active');
+    //         }  else {
+    //             $('.dropdown-menu').removeClass('active');
+    //             $(this).children('.dropdown-menu').addClass('active');
+    //         }
+    // });
+
+
+
+
+    // ESERCIZIO CREATO CON SLIDEUP SLIDEDOWN
+    $('.menu-item').click(function() {
+        if ($(this).children('.dropdown-menu').is(':visible')) {
+                $('.dropdown-menu').slideUp(300);
+            } else {
+                $('.dropdown-menu').slideUp(300);
+                $(this).children('.dropdown-menu').slideDown(300);
+            }
+    });
+
+
+
+
+
+
 });
